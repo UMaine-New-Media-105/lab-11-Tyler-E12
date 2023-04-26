39791cbe5e4e1bf2c55a6cd593aa13305d168670
Tyler-E12 <124187250+Tyler-E12@users.noreply.github.com>
2023-04-26 12:16:59 -0400

    Update challengeTwo.js

--- challengeTwo.js
+++ challengeTwo.js
@@ -1,5 +1,20 @@
 //pseudocode for asteroid game
+/*
+suggestions:
+I think it would be interesting for the asteroids to have differently colored
+outlines.
 
+Maybe add some stars to the background
+
+Give the ship a differently colored outline every time you spawn in.
+
+The controls for the spaceship are a bit slippery, maybe have the inputs be
+a bit more exact.
+
+Keeping track of how many asteroids you destroyed would be good.
+
+Have te ship get destroyed if you hit an asteroid.
+*/
 function setup() {
   createCanvas(400, 400);
 }
@@ -10,16 +25,16 @@ function draw() {
   /*
   call in the asteroids and spaceship.
   
-  using the dist()function, create code that determines the distance between the x and y value of the spaceship shot, and the x and y of one of the asteroids.
-  Add an if statement, stating that if the shot hits the asteroid, the smaller asteroid sprites are called.
+  using the dist()function, create code that determines the distance between the x and y value of the spaceship shot, and the x and y of one of the asteroids. Add an
+  if statement, stating that if the shot hits the asteroid, the smaller asteroid sprites are called.
   
   */
 }
 
 /*
 create a function to create the shape of the spaceship the player controls, in this function, also include a KeyPressed function, allowing the player to actually control it.
-add and if statement that  creates the shot from the spaceship, so that if the spacebar is pressed, the shot continually moves foreward by adding a variable to it repeatedly.
-(for example, adding 1 until its equal to 401, so its off the canvas) have the starting location of the shot be equal to the tip of the ship.
+add and if statement that  creates the shot from the spaceship, so that if the spacebar is pressed, the shot continually moves foreward by adding a variable
+to it repeatedly. (for example, adding 1 until its equal to 401, so its off the canvas) have the starting location of the shot be equal to the tip of the ship.
 
 
 
